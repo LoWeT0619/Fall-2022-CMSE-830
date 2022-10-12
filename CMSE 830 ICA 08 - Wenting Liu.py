ca4a7abb355319eec801f12274c057c7c61d0f79
LoWeT0619 <89941453+LoWeT0619@users.noreply.github.com>
2022-10-12 15:47:46 -0400

    Update CMSE 830 ICA 08 - Wenting Liu.py

--- CMSE 830 ICA 08 - Wenting Liu.py
+++ CMSE 830 ICA 08 - Wenting Liu.py
@@ -70,10 +70,7 @@ values=[]
 # improvement: make the min and max values correspond to what the user selected
 for parameter, parameter_df in zip(parameter_list, parameter_default_values):
  
-    values = st.sidebar.slider(label=parameter, key=parameter, value=float(parameter_df),
-                               min_value=parameter_df.min(axis=0),
-                               max_value=parameter_df.max(axis=0),
-                               step=0.1)
+    values = st.sidebar.slider(label=parameter, key=parameter, value=float(parameter_df), min_value=-10.0, max_value=10.0, step=0.1)
     parameter_input_values.append(values)
  
 input_variables = pd.DataFrame([parameter_input_values], columns=parameter_list, dtype=float)
